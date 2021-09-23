428336fbd2100c44394b6aa34a9ca26fdcf7b2fb
BOHDAN1329 <bpankiv011@gmail.com>
2021-09-24 00:49:20 +0300

    first branch_02 commit

--- Source.cpp
+++ Source.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int main()
 {
 	double m; // віхдний параметр
-	double z1; // результат обчислення першого виразу
+	//double z1; // результат обчислення першого виразу
 	double z2; // результат обчислення другого виразу
 	cout << "m = "; cin >> m;
-	z1 = (sqrt((3 * m + 2) * (3 * m + 2) - 24 * m)) / (3 * sqrt(m) - 2. / sqrt(m));
+	//z1 = (sqrt((3 * m + 2) * (3 * m + 2) - 24 * m)) / (3 * sqrt(m) - 2. / sqrt(m));
 	z2 = sqrt(m);
-	cout << "z1 = " << z1 << endl;
+	//cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 	cin.get();
 	return 0;
