09934dc763e35fb84ecf69285ebd445be35da37d
BOHDAN1329 <bpankiv011@gmail.com>
2021-09-24 00:33:43 +0300

    branch_01

---
+++ Source.cpp
@@ -0,0 +1,34 @@
+//Lab_02.tpp
+// < Паньків Богдан >
+// Лабораторна робота №2.1
+// Лінійні Прорграми
+// Варіант 17
+# include <iostream>
+# include <cmath>
+using namespace std;
+int main()
+{
+	double m; // віхдний параметр
+	double z1; // результат обчислення першого виразу
+	double z2; // результат обчислення другого виразу
+	cout << "m = "; cin >> m;
+	z1 = (sqrt((3 * m + 2) * (3 * m + 2) - 24 * m)) / (3 * sqrt(m) - 2. / sqrt(m));
+	z2 = sqrt(m);
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
